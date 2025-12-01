f98fc36dd9374a6589ef909a410f543447b33888
chris <123799877+xleonx0x@users.noreply.github.com>
2025-12-02 06:31:40 +1100

    day1

--- 2025/day1/part1.go
+++ 2025/day1/part1.go
@@ -1,6 +1,35 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"os"
+	"strings"
+    "strconv"
+)
 
 func part1() {
-    fmt.Println("Hello, Wowwwrld!")
+    filePath := "input"
+
+    content, _ := os.ReadFile(filePath)
+
+    contentStr := string(content)
+    ans := 0
+    currDeg := 50
+
+    for _, str := range strings.Split(contentStr, "\n") {
+        dir, numStr := str[0:1], str[1:]
+        num, _ := strconv.Atoi(numStr)
+        switch dir {
+        case "L":
+            currDeg = (currDeg - num) % 100
+        case "R":
+            currDeg = (currDeg + num) % 100
+        }
+
+        if currDeg == 0 {
+            ans++
+        }
+    }
+
+    fmt.Println(ans)
 }
--- 2025/day1/part2.go
+++ 2025/day1/part2.go
@@ -1,6 +1,41 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"os"
+	"strings"
+    "strconv"
+)
 
 func part2() {
-    fmt.Println("Hello, World!")
+    filePath := "input"
+
+    content, _ := os.ReadFile(filePath)
+
+    contentStr := string(content)
+    ans := 0
+    currDeg := 50
+
+    for str := range strings.SplitSeq(contentStr, "\n") {
+        dir, numStr := str[0:1], str[1:]
+        num, _ := strconv.Atoi(numStr)
+        wholeHundreds := num / 100
+        remainder := num - 100 * wholeHundreds
+        switch dir {
+        case "L":
+            if currDeg - remainder <= 0 && currDeg != 0 {
+                ans += 1
+            }
+            currDeg = ((currDeg - num) % 100 + 100) % 100
+        case "R":
+            if currDeg + remainder >= 100 {
+                ans += 1
+            }
+            currDeg = ((currDeg + num) % 100 + 100) % 100
+        }
+
+        ans += wholeHundreds
+    }
+
+    fmt.Println(ans)
 }
